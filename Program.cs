8de7410e635c48be50e952ac6bbb098cb92b8174
muhammadrinaldy <muhammad.rinaldy@bni.co.id>
2025-11-19 08:03:37 +0700

    update code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Masukan Angka : ");
+            string input = Console.ReadLine() ?? "";
+
+            string getinput = new string(input.Where(char.IsDigit).ToArray());
+
+            if (string.IsNullOrWhiteSpace(getinput))
+            {
+                Console.WriteLine("input tidak valid, Masukan angka");
+                continue;
+            }
+
+            int number = int.Parse(getinput);
+
+            string reserveinput = new string(getinput.Reverse().ToArray());
+            int reservenumber = int.Parse(reserveinput);
+
+            int difference = Math.Abs(number - reservenumber);
+
+            Console.WriteLine($"Angka Inputan         : {number}");
+            Console.WriteLine($"Kebalikan angka input : {reservenumber}");
+            Console.WriteLine($"Selesih               : {difference}");
+        }
+    }
+}
